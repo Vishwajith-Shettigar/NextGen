31fa09162d0f1573b610b6d1273fed9d5e4b8290
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-05-25 09:49:28 +0530

    signup and login

--- app/src/main/java/com/example/nextgen/Activity/ActivityComponent.kt
+++ app/src/main/java/com/example/nextgen/Activity/ActivityComponent.kt
@@ -4,12 +4,14 @@ import com.example.nextgen.Application.AppModule
 import com.example.nextgen.Fragment.FragmentComponent
 import com.example.nextgen.Fragment.FragmentModule
 import com.example.nextgen.home.HomeActivity
+import com.example.nextgen.signup.SignupActivity
 import dagger.Subcomponent
 
 @ActivityScope
 @Subcomponent(modules = [ActivityModule::class])
 interface ActivityComponent {
   fun inject(activity: HomeActivity)
+  fun inject(activity: SignupActivity)
 
   fun fragmentComponent(fragmentModule: FragmentModule): FragmentComponent
 }
