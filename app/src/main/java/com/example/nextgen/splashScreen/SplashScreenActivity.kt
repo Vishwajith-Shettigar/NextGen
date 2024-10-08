c60b877f71677acc65cb13fd208e05dd25fc8e3b
Adarsh Kumar Maurya <amaurya4570@gmail.com>
2024-10-08 23:02:34 +0530

    check current in SplashScreenActivity instead of SignupSigninActivity

--- app/src/main/java/com/example/nextgen/splashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/nextgen/splashScreen/SplashScreenActivity.kt
@@ -5,24 +5,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.nextgen.R
+import com.example.nextgen.home.HomeActivity
 import com.example.nextgen.signup.SignupSigninActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+
         lifecycleScope.launch {
             delay(3000) // 3-second delay
-            navigateToMainActivity()
+            redirectUserBasedOnSignInStatus()
         }
     }
 
-    private fun navigateToMainActivity() {
-        startActivity(Intent(this, SignupSigninActivity::class.java))
+    private fun redirectUserBasedOnSignInStatus() {
+        if (firebaseAuth.currentUser != null) {
+            startActivity(HomeActivity.createHomeActivity(this))
+        } else {
+            startActivity(Intent(this, SignupSigninActivity::class.java))
+        }
         finish()
     }
 }
